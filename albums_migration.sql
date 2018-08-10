bc74ffee5358f7dcee30fc2f42ab53449768591d
zaneagley <zaneagley@gmail.com>
2018-08-10 09:43:16 -0500

    added database exercise and first script

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50),
+name VARCHAR (50),
+release_date DATE,
+sales FLOAT(11,2),
+genre VARCHAR(10)
+);
